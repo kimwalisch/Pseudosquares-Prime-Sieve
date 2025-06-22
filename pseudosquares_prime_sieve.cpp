0390fe9ee1612698298bdbd1aa1720f14043be00
Kim Walisch <kim.walisch@gmail.com>
2025-06-22 18:12:52 +0200

    Add Copyright

--- pseudosquares_prime_sieve.cpp
+++ pseudosquares_prime_sieve.cpp
@@ -1,3 +1,18 @@
+///
+/// @file   pseudosquares_prime_sieve.cpp
+/// @brief  This is a C++ implementation of J. P. Sorenson's
+///         Pseudosquares Prime Sieve algorithm for 64-bit integers.
+///         The Pseudosquares Prime Sieve uses much less memory
+///         than most other prime sieving algorithms: it has a
+///         conjectured runtime complexity of O(n log n) operations
+///         and uses O((log n)^2) space.
+///
+/// Copyright (C) 2025 Kim Walisch, <kim.walisch@gmail.com>
+///
+/// This file is distributed under the BSD License. See the COPYING
+/// file in the top level directory.
+///
+
 #include <iostream>
 #include <cmath>
 #include <cstdlib>
